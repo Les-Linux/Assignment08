c4cdaf1a46013b2071eb6b0874de2f718585ca75
Leslie Fleming <leslie_fleming@elbicon.com>
2021-09-26 12:59:01 +0100

    1.0.0

--- src/com/elbicon/coderscampus/ListOfNumbers.java
+++ src/com/elbicon/coderscampus/ListOfNumbers.java
@@ -10,7 +10,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
-import static java.lang.Thread.sleep;
 
 public class ListOfNumbers {
     TaskDto taskDto = new TaskDto();
