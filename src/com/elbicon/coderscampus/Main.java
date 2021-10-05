5bd9442ec21338a813540e765c698bf4b6445b95
Leslie Fleming <leslie_fleming@elbicon.com>
2021-10-05 22:32:47 +0100

    1.0.0

---
+++ src/com/elbicon/coderscampus/Main.java
@@ -0,0 +1,13 @@
+package com.elbicon.coderscampus;
+
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ListOfNumbers ListOfNumbers = new ListOfNumbers();
+        TaskDto taskDto = null;
+        ListOfNumbers.read();
+
+    }
+}
